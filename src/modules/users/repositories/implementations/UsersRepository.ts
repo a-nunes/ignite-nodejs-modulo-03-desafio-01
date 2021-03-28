ffd7457120a1a2a36d4cd8fb697a1d20b6df80ab
Artur Nunes <art.nunes@gmail.com>
2021-03-28 09:40:13 -0300

    All tests working by the rules.

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,25 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne(user_id, {relations: ["games"]});
+
+    if(!user) {
+      throw new Error("User does not exists")
+    }
+
+    return user;
+    
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM USERS ORDER BY first_name ASC`);
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM USERS WHERE first_name = INITCAP('${first_name}') AND last_name =  INITCAP('${last_name}')`);
+
   }
 }
